d3258db552bdcf0b3b795618ead2d91e7ad4fe08
piercenovo <piercenovoah@gmail.com>
2024-03-30 17:16:07 -0500

    feat: add Navbar Component

---
+++ components/navbar.tsx
@@ -0,0 +1,57 @@
+'use client'
+
+import Link from 'next/link'
+import logo from '../public/logo.png'
+import { SearchIcon } from '@/components/icons/search'
+import { useState } from 'react'
+import Image from 'next/image'
+import { cn } from '@/lib/utils'
+import { EditIcon } from './icons/edit'
+
+export const Navbar = () => {
+  const [searchBoxVisibility, setSearchBoxVisibility] = useState(false)
+
+  return (
+    <nav className='navbar'>
+      <Link href='/' className='w-10 flex-none'>
+        <Image src={logo} alt='Logo' className='fill' />
+      </Link>
+
+      <div
+        className={cn(
+          'border-grey md:show absolute left-0 top-full mt-0.5 w-full border-b bg-white px-[5vw] py-4 md:relative md:inset-0 md:block md:w-auto md:border-0 md:p-0',
+          searchBoxVisibility ? 'show' : 'hide'
+        )}
+      >
+        <input
+          type='text'
+          placeholder='Search'
+          className='bg-grey placeholder:text-dark-grey w-full rounded-full p-4 pl-6 pr-[12%] md:w-auto md:pl-12 md:pr-6'
+        />
+
+        <SearchIcon className='text-dark-grey absolute right-[10%] top-1/2 size-5 -translate-y-1/2 md:pointer-events-none md:left-5' />
+      </div>
+
+      <div className='ml-auto flex items-center gap-3 md:gap-4'>
+        <button
+          onClick={() => setSearchBoxVisibility((currentVal) => !currentVal)}
+          className='bg-grey flex size-12 items-center justify-center rounded-full md:hidden'
+        >
+          <SearchIcon className='size-5' />
+        </button>
+
+        <Link href='/editor' className='link hidden gap-2 md:flex'>
+          <EditIcon className='size-6' />
+          <span>Write</span>
+        </Link>
+
+        <Link href='/signin' className='btn-dark py-2'>
+          Sign In
+        </Link>
+        <Link href='/signup' className='btn-light hidden py-2 md:block'>
+          Sign Up
+        </Link>
+      </div>
+    </nav>
+  )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,33 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    colors: {
+      white: '#FFFFFF',
+      black: '#242424',
+      grey: '#F3F3F3',
+      'dark-grey': '#6B6B6B',
+      red: '#FF4E4E',
+      transparent: 'transparent',
+      twitter: '#1DA1F2',
+      purple: '#8B46FF'
     },
+
+    fontSize: {
+      sm: '12px',
+      base: '14px',
+      xl: '16px',
+      '2xl': '20px',
+      '3xl': '28px',
+      '4xl': '38px',
+      '5xl': '50px'
+    }
   },
-  plugins: [],
-};
-export default config;
+  plugins: []
+}
+export default config
